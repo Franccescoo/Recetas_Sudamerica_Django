6c5ccf74dabc171c21189746a2b63f3a1e5fad85
NodyDuoc <ale.lara@duocuc.cl>
2022-05-22 18:05:25 -0400

    Urls 1

--- Recetas_Sudamerica/Recetas/urls.py
+++ Recetas_Sudamerica/Recetas/urls.py
@@ -1,6 +1,16 @@
 from django.urls import URLPattern, path
-from .views import  index
+from .views import  index, Login, registrarse, contact, Vista_de_Admin, Ver_Receta_Admin, Ver_Usuario_Admin,Editar_Recetas_Admin, menu
 
 urlpatterns = [
     path('',index,name="index"),
+    path('Login/',Login,name="Login"),
+    path('registrarse/',registrarse,name="registrarse"),
+    path('contact/',contact,name="contact"),
+    path('Vista_de_Admin/',Vista_de_Admin,name="Vista_de_Admin"),
+    path('Ver_Receta_Admin/',Ver_Receta_Admin,name="Ver_Receta_Admin"),
+    path('Ver_Usuario_Admin/',Ver_Usuario_Admin,name="Ver_Usuario_Admin"),
+    path('Editar_Recetas_Admin/',Editar_Recetas_Admin,name="Editar_Recetas_Admin"),
+    path('menu/',menu,name="menu"),
+    
+
 ]
--- Recetas_Sudamerica/Recetas/views.py
+++ Recetas_Sudamerica/Recetas/views.py
@@ -4,3 +4,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'Recetas/index.html')
+
+def Login(request):
+    return render(request,'Recetas/Login.html')
+
+def registrarse(request):
+    return render(request,'Recetas/registrarse.html')
+
+def contact(request):
+    return render(request,'Recetas/contact.html')
+
+def Vista_de_Admin(request):
+    return render(request,'Recetas/Vista_de_Admin.html')
+
+def Ver_Receta_Admin(request):
+    return render(request,'Recetas/Ver_Receta_Admin.html')
+
+def Ver_Usuario_Admin(request):
+    return render(request,'Recetas/Ver_Usuario_Admin.html')
+
+def Editar_Recetas_Admin(request):
+    return render(request,'Recetas/Editar_Recetas_Admin.html')
+
+def menu(request):
+    return render(request,'Recetas/menu.html')
