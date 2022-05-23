1d4da653ededf76ed6a28e2c766a6e991d5c2d8a
ZHEKO <fr.artuso@duocuc.cl>
2022-05-23 15:50:00 -0400

    Cambio de carpetas nuevamente, NO TOCAR

---
+++ Recetas_Sudamerica/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Recetas_Sudamerica project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recetas_Sudamerica.settings')
+
+application = get_asgi_application()
