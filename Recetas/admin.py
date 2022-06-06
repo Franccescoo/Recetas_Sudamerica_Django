15c7d01c3da1fb6d318a91ea3d85631f5d2f016c
ZHEKO <fr.artuso@duocuc.cl>
2022-06-06 13:37:13 -0400

    Links de Receta en los html

--- Recetas/admin.py
+++ Recetas/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Usuario, Nacionalidad, Receta, RolUsuario
+from .models import Usuario, Nacionalidad, Receta, RolUsuario, Comentario
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Nacionalidad)
 admin.site.register(Receta)
 admin.site.register(RolUsuario)
-
+admin.site.register(Comentario)
