2ffa11ad5358b2df59edb055656117383394d074
NodyDuoc <ale.lara@duocuc.cl>
2022-06-08 01:33:29 -0400

    A dormir

--- Recetas_Sudamerica/settings.py
+++ Recetas_Sudamerica/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': '127.0.0.1:1521/xe',
-        'USER': 'Receta',
+        'USER': 'RecetaPresentacion3',
         'PASSWORD': 'oracle',
         'TEST':{
             'USER': 'default_test',
