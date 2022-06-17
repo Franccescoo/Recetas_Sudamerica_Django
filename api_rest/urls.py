9f1de483861a119e55116f7467f292bd3994a2b1
NodyDuoc <ale.lara@duocuc.cl>
2022-06-17 09:32:11 -0400

    Recetas y comentarios en api_rest

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from api_rest.views import listado_usuarios, addUsuario, modEliminarUsuario
+from api_rest.views import listado_usuarios, addUsuario, modEliminarUsuario, listado_recetas, addReceta,modEliminarReceta
 from api_rest.viewsLogin import ini_user
 urlpatterns = [
+    path('ini_user/',ini_user,name="ini_user"),
+
     path('listado_usuarios/',listado_usuarios,name="listado_usuarios"),
     path('addUsuario/',addUsuario,name="addUsuario"),
     path('modEliminarUsuario/<id>',modEliminarUsuario,name="modEliminarUsuario"),
-    path('ini_user/',ini_user,name="ini_user"),
+
+    path('listado_recetas/',listado_recetas,name="listado_recetas"),
+    path('addReceta/',addReceta,name="addReceta"),
+    path('modEliminarReceta/<id>',modEliminarReceta,name="modEliminarReceta"),
     
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -4,13 +4,14 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
-from Recetas.models import Usuario
+from Recetas.models import Comentario, Receta, Usuario
 from .serializers import UsuarioSerializers,RecetaSerializers,ComentarioSerializers
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
+# Usuarios
 @csrf_exempt
 @api_view(['GET','POST'])
 @permission_classes((IsAuthenticated,))
@@ -64,4 +65,110 @@ def modEliminarUsuario(request,id):
         m.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
     
+# Recetas
+@csrf_exempt
+@api_view(['GET','POST'])
+@permission_classes((IsAuthenticated,))
+def listado_recetas(request):
+    if request.method == 'GET':
+        recetas = Receta.objects.all()
+        serializer = RecetaSerializers(recetas,many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = RecetaSerializers(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+def addReceta(request):
+    data = JSONParser().parse(request)
+    serializer = RecetaSerializers(data = data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
+def modEliminarReceta(request,id):
+    try:
+        m = Receta.objects.get(idReceta = id)
+    except Receta.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = RecetaSerializers(m)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        data2 = JSONParser().parse(request)
+        serializer = RecetaSerializers(m, data = data2)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        m.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+# Comentarios
+@csrf_exempt
+@api_view(['GET','POST'])
+@permission_classes((IsAuthenticated,))
+def listado_comentarios(request):
+    if request.method == 'GET':
+        comentarios = Comentario.objects.all()
+        serializer = ComentarioSerializers(comentarios,many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ComentarioSerializers(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+def addComentario(request):
+    data = JSONParser().parse(request)
+    serializer = ComentarioSerializers(data = data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
+def modEliminarComentario(request,id):
+    try:
+        m = Comentario.objects.get(idComentario = id)
+    except Comentario.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = ComentarioSerializers(m)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        data2 = JSONParser().parse(request)
+        serializer = ComentarioSerializers(m, data = data2)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        m.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
