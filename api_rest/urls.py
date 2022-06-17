85d0f1b82b0d1936628de79f4922cbccc6b89c2a
NodyDuoc <ale.lara@duocuc.cl>
2022-06-16 22:42:43 -0400

    Bajo url

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from api_rest.views import listado_mascotas, addMascota, modEliminarMascota
-from api_rest.viewsLogin import ini_user
+#from api_rest.views import listado_mascotas, addMascota, modEliminarMascota
+#from api_rest.viewsLogin import ini_user
 urlpatterns = [
-    path('listado_mascotas/',listado_mascotas,name="listado_mascotas"),
-    path('addMascota/',addMascota,name="addMascota"),
-    path('modEliminarMascota/<codigo>',modEliminarMascota,name="modEliminarMascota"),
-    path('ini_user/',ini_user,name="ini_user"),
+    
 ]
